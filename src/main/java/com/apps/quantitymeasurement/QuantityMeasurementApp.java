0526c75a7b71e498289a5b297904fe063eb96931
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2026-02-22 23:22:18 +0530

    [Divyansh Pandey]: Added Quantity Measurement program

---
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,12 @@
+package com.apps.quantitymeasurement;
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Are equal? " + f1.equals(f2));
+    }
+}
