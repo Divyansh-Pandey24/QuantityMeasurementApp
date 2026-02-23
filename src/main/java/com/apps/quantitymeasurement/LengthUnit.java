4d97f0b489004eebb129610c827eee48082faea3
Divyansh-Pandey24 <divyanshpandey996@gmail.com>
2026-02-23 09:15:38 +0530

    [Divyansh Pandey]: Implemented unit to unit conversion

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -2,18 +2,15 @@ package com.apps.quantitymeasurement;
 
 public enum LengthUnit {
 
-    FEET(12.0),
-    INCHES(1.0),
-    YARDS(36.0),
-    CENTIMETERS(0.393701);
+	FEET(12.0), INCHES(1.0), YARDS(36.0), CENTIMETERS(0.393701);
 
-    private final double conversionFactorToInches;
+	private final double conversionFactorToInches;
 
-    LengthUnit(double conversionFactorToInches) {
-        this.conversionFactorToInches = conversionFactorToInches;
-    }
+	LengthUnit(double conversionFactorToInches) {
+		this.conversionFactorToInches = conversionFactorToInches;
+	}
 
-    public double getConversionFactor() {
-        return conversionFactorToInches;
-    }
+	public double getConversionFactor() {
+		return conversionFactorToInches;
+	}
 }
